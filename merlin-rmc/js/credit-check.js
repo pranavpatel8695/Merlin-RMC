42fcd16493e6eeeaf07ebc60a2bff846b0245615
pranavpatel8695 <pranavpatel8695@gmail.com>
2026-02-27 19:57:20 +0530

    Delete merlin-rmc directory

--- merlin-rmc/js/credit-check.js
+++
@@ -1,7 +0,0 @@
-export function showCreditPopup(customer) {
-  if (customer.overdueDays > 0) {
-    alert(
-      `PAYMENT REMINDER\n\nCustomer: ${customer.name}\nOverdue By: ${customer.overdueDays} days`
-    );
-  }
-}
