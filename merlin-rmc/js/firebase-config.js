bd9202ebb44dd551820b0adaafd0ea86b8ab64f2
pranavpatel8695 <pranavpatel8695@gmail.com>
2026-02-27 19:25:15 +0530

    Add files via upload

--- merlin-rmc/js/firebase-config.js
+++ merlin-rmc/js/firebase-config.js
@@ -1,7 +1,17 @@
-firebase-config.js
-auth.js
-role-check.js
-credit-check.js
-admin.js
-manager.js
-operator.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
